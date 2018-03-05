ea85826831a6393148692572d559131071137628
JoonYong <joonyong.jinong@gmail.com>
2018-03-05 12:27:15 +0900

    CvtRawCommand 추가

--- spec/cvtcommand.h
+++ spec/cvtcommand.h
@@ -29,14 +29,15 @@ public:
     /**
      새로운 명령를 생성한다.
      @param cmdid 명령의 아이디
-     @param pdev  명령을 수행할 장비의 포인터
+     @param pdev  명령을 수행할 장비스펙의 포인터
     */
     CvtCommand(int cmdid, CvtDeviceSpec *pdevspec) {
         _id = cmdid;
         _pdevspec = pdevspec;
     }
 
-    ~CvtCommand();
+    ~CvtCommand() {
+    }
 
     /**
      명령에 부여된 아이디를 리턴한다.
@@ -71,7 +72,19 @@ public:
 class CvtRatioCommand : public CvtCommand {
 private:
     double _ratio;          //< 타겟 비율. 0~1 사이값으로 한다.
+
 public:
+    /**
+     새로운 명령를 생성한다.
+     @param cmdid 명령의 아이디
+     @param pdev  명령을 수행할 장비스펙의 포인터
+     @param ratio 지정된 비율
+    */
+    CvtRatioCommand(int cmdid, CvtDeviceSpec *pdevspec, double ratio) 
+        : CvtCommand (cmdid, pdevspec) {
+        _ratio = ratio;
+    }
+
     /**
      지정된 비율값을 확인한다.
      @return ratio 값
@@ -79,15 +92,55 @@ public:
     double getratio () {
         return _ratio;
     }
+};
+
+/*
+ @brief CvtRawCommand 는 특정 장비에게 전달되는 명령으로, 특별한 기능을 가진 장비에게 전달하는 메세지 이다. 
+*/
+class CvtRawCommand : public CvtCommand {
+private:
+    int _modelcode;         // 명령의 인자를 해석할 수 있는 드라이버의 모델코드
+    string _devid;          // 명령을 처리할 장비의 ID
+    string _base64arg;      // 전달할 명령의 인자. 바이너리인경우를 고려하여 base64인코딩을 수행.
+    
+public:
+    /**
+     새로운 명령를 생성한다.
+     @param cmdid 명령의 아이디
+     @param pdev  명령을 수행할 장비스펙의 포인터
+     @param modelcode 명령의 인자를 해석할 수 있는 드라이버의 모델코드
+     @param devid 명령을 수행할 장비의 아이디
+     @param arg 명령의 인자. 문자열 형으로 꼭 base64 인코딩 되어 있어야 함.
+    */
+    CvtRawCommand(int cmdid, CvtDeviceSpec *pdevspec, int modelcode, string devid, string arg) 
+        : CvtCommand (cmdid, pdevspec) {
+        _modelcode = modelcode;
+        _devid = devid;
+        _base64arg = arg;
+    }
 
     /**
-     지정된 비율값을 세팅한다.
-     @param ratio
-     @return 세팅된 비율값
+     명령에 할당된 모델코드를 확인한다.
+     @return 모델코드값
     */
-    double setratio (double ratio) {
-        _ratio = ratio;
-        return _ratio;
+    int getmodelcode () {
+        return _modelcode;
+    }
+
+    /**
+     명령을 수행할 장비의 아이디를 확인한다.
+     @return 장비아이디값
+    */
+    string getdevid () {
+        return _devid;
+    }
+
+    /**
+     명령 수행을 위한 인자를 확인한다.
+     @return 명령 인자값
+    */
+    string getarg() {
+        return _base64arg;
     }
 };
 
